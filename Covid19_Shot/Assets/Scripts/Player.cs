6517c4b7bb31ed0a52722efdf7aeeb2f9a3c1aad
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-05 12:07:16 +0900

    플레이어 이동
    충돌시 떨림을 방지하기 위해서는 움직임 관련 코드를 FixedUpdate에 넣어주면 된다

--- Covid19_Shot/Assets/Scripts/Player.cs
+++ Covid19_Shot/Assets/Scripts/Player.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] int speed;
+
+    private float moveX;
+    private float moveY;
+
+    Rigidbody2D rigidbody;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(moveX, moveY, 0) * speed * Time.deltaTime;
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        Vector2 moveMentVector = inputValue.Get<Vector2>();
+        moveX = moveMentVector.x;
+        moveY = moveMentVector.y;
+
+    }
+
 }
