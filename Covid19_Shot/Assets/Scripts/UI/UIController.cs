bfa21eb9885f5bac833e7aa02949278632611347
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-07 18:03:45 +0900

    플레이어 점수 UI
    기존 GaugeController.cs 를 UIController로 변경

--- Covid19_Shot/Assets/Scripts/UI/UIController.cs
+++ Covid19_Shot/Assets/Scripts/UI/UIController.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GaugeController : MonoBehaviour
+public class UIController : MonoBehaviour
 {
+
+    [Header("Gauge")]
     [SerializeField] Image HPImg;
     [SerializeField] Image painImg;
-
     [SerializeField] Text HPText;
     [SerializeField] Text painText;
-
     float HP;
     float pain;
+
+    [Header("Score")]
+    [SerializeField] Text scoreText;
 
     Player player => SystemManager.Instance.Player;
 
@@ -22,6 +25,7 @@ public class GaugeController : MonoBehaviour
         painImg = painImg.GetComponent<Image>();
         HPText = HPText.GetComponent<Text>();
         painText = painText.GetComponent<Text>();
+        scoreText = scoreText.GetComponent<Text>();
     }
 
 
@@ -36,6 +40,8 @@ public class GaugeController : MonoBehaviour
 
         HPText.text = player.HP.ToString();
         painText.text = player.pain.ToString();
+
+        scoreText.text =  "score : "  + player.score.ToString();
 
     }
 }
