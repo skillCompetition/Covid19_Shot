6229a7036205214582b4bccb9efca5245d7a6dd6
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-07 20:55:12 +0900

    보스 공격_ 원형의 공격

--- Covid19_Shot/Assets/Scripts/Enemy/Boss.cs
+++ Covid19_Shot/Assets/Scripts/Enemy/Boss.cs
@@ -30,29 +30,55 @@ public class Boss : Enemy
 
     protected override IEnumerator Attack()
     {
-        StartCoroutine("Barrage");
+            if (HP <= 0)
+                StopCoroutine("Attack");
 
+            StartCoroutine("Barrage");
+            yield return new WaitForSeconds(0.3f);
+            StartCoroutine("Circle");
 
-        yield return null;
     }
 
+    /// <summary>
+    /// 부채형태의 연속공격
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Barrage()
     {
+        if (HP <= 0)
+            yield break;
         int bulletNum = 101;
 
         for (int i = 0; i < bulletNum; i++)
         {
             GameObject bullet = Instantiate(this.bullet, transform.position, transform.rotation);
-            Debug.Log("bullet : " + bullet.transform.position);
 
-            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 2 * i / bulletNum), -1);
+            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 10 * i / bulletNum), -1);
 
             bullet.GetComponent<Rigidbody2D>().AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
 
             yield return new WaitForSeconds(0.2f);
         }
     }
-    
+
+    IEnumerator Circle()
+    {
+
+        int bulletNum = 201;
+
+        for (int i = 0; i < bulletNum; i++)
+        {
+            GameObject bullet = Instantiate(this.bullet, transform.position, transform.rotation);
+
+            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 2 * i / bulletNum), 
+                                         Mathf.Sin(Mathf.PI * 2 * i / bulletNum));
+
+            bullet.GetComponent<Rigidbody2D>().AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
