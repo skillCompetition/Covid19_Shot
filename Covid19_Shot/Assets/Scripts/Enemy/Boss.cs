b38525960956dc0a3beeb25372fccd73e4631974
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-07 20:08:42 +0900

    보스 공격 _ 부채 형태의 연속공격

--- Covid19_Shot/Assets/Scripts/Enemy/Boss.cs
+++ Covid19_Shot/Assets/Scripts/Enemy/Boss.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class Boss : Enemy
 {
+    GameObject bullet;
     protected override void Awake()
     {
         base.Awake();
+        bullet = Resources.Load("Bullet/Boss Bullet") as GameObject;
     }
 
     protected override void Start()
     {
         base.Start();
         Invoke("Stop", 2f);
+        StartCoroutine("Attack");
     }
 
     private void Stop()
     {
         rigid.velocity = Vector2.zero;
-        Debug.Log("»Ð");
     }
 
     protected override void Update()
@@ -26,10 +28,31 @@ public class Boss : Enemy
         base.Update();
     }
 
-    /*protected override IEnumerator Attack()
+    protected override IEnumerator Attack()
     {
+        StartCoroutine("Barrage");
 
-    }*/
+
+        yield return null;
+    }
+
+    IEnumerator Barrage()
+    {
+        int bulletNum = 101;
+
+        for (int i = 0; i < bulletNum; i++)
+        {
+            GameObject bullet = Instantiate(this.bullet, transform.position, transform.rotation);
+            Debug.Log("bullet : " + bullet.transform.position);
+
+            Vector2 dirVec = new Vector2(Mathf.Cos(Mathf.PI * 2 * i / bulletNum), -1);
+
+            bullet.GetComponent<Rigidbody2D>().AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+    
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
