a004d0609e120d110e97a47b97153845efbfef35
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-12 15:12:49 +0900

    Txt 파일 파싱

---
+++ Covid19_Shot/Assets/Scripts/Spawn/Parce.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Parce : MonoBehaviour
+{
+    List<Spawn> spawnList = new List<Spawn>();
+
+    void Awake()
+    {
+        ReadSpawnFile();
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+
+    void ReadSpawnFile()
+    {
+        
+        spawnList.Clear();
+
+        //TextAsset : 텍스트 파일 에셋 클래스
+        TextAsset textFile = Resources.Load("Stage1") as TextAsset;
+        //StringReader : 파일 내의 문자열 데이터 읽기 클래스
+        StringReader stringReader = new StringReader(textFile.text);
+
+        while(stringReader != null)
+        {
+            //ReadLine() = 텍스트 데이터를 한 줄 씩 반환
+            string line = stringReader.ReadLine();
+            Debug.Log(line);
+
+            if (line == null)
+                return;
+
+            Spawn spawnData = new Spawn();
+
+            spawnData.type = line.Split(',')[0];
+            spawnData.point = int.Parse(line.Split(',')[1]);
+            spawnData.delay = float.Parse(line.Split(',')[2]);
+
+            spawnList.Add(spawnData);
+
+
+        }
+
+        //stringReader로 열어둔 파일은 작업 후 꼭 닫기
+        stringReader.Close();
+
+
+
+
+    }
+}
---
+++ Covid19_Shot/Assets/Scripts/Spawn/Spawn.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn
+{
+    public string type;
+    public int point;
+    public float delay;
+}
