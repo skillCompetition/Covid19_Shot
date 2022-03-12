cde3b96b0647280cca08cc04ffed8661bfe25a67
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-03-12 13:22:24 +0900

    GameScene 재시작 기능

--- Covid19_Shot/Assets/Scripts/GameManager.cs
+++ Covid19_Shot/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,7 +13,8 @@ public class GameManager : MonoBehaviour
     public int score;
     public int pain;
 
-
+    [SerializeField] GameObject gameOverCanvas;
+    [SerializeField] Text scoreText;
 
     void Awake()
     {
@@ -21,6 +24,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameOverCanvas.SetActive(false);
+        Time.timeScale = 1;
         HP = (int)MaxHP;
         pain = 30;
 
@@ -29,6 +34,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(HP <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public string thisGameScene;
+    void GameOver()
+    {
+        thisGameScene = SceneManager.GetActiveScene().name;
+        Time.timeScale = 0;
+        gameOverCanvas.SetActive(true);
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void RestartBtnCilck()
+    {
+        SceneManager.LoadScene(thisGameScene);
     }
 }
